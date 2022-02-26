50c42a66ae033623f78870275d58870d3e542db2
RonKu01 <87351766+RonKu01@users.noreply.github.com>
2022-02-26 12:39:52 +0800

    0226 12.30pm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, {Component} from 'react'
-import Table from './Table'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-                <Table />
-            </div>
-        )
+            <h1>test</h1>
+        );
     }
 }
 
