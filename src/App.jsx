6935c851cf01405de63f418005effa79b4317265
Calvin Goh <0132005@kdu-online.com>
2022-03-01 21:40:24 +0800

    0103 940pm navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Main from "./components/Main";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
+import Navbar from "./components/navbar/Navbar"
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Main />} />
                     <Route path="/login" element={<Login />} />
---
+++ src/components/navbar/MenuItems.jsx
@@ -0,0 +1,24 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'My Appointments',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Register',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+
+]
