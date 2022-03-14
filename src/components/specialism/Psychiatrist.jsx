cdc3a0ae13e4cac8338bea04308ddd6faf7a9aaf
danesshmathura <danessh75@gmail.com>
2022-03-15 02:18:26 +0800

    update Psychiatrist.jsx file

--- src/components/specialism/Psychiatrist.jsx
+++ src/components/specialism/Psychiatrist.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../navbar/Navbar_Patient";
 import "../dashboard/dashboard.scss";
-import { Carousel} from "react-bootstrap";
+import { Card} from "react-bootstrap";
 
 
 
@@ -12,21 +12,109 @@ function Psychiatrist() {
 
             <h1>List of Psychiatrist Doctors</h1>
              
-            <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-          </Carousel>
+            <Card style={{ width: '100rem', height: '20rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Dr Ting Joe Hang</Card.Title>
+    <Card.Text>
+      <h6><b>Specialisation:</b> Psychiatry</h6>
+      <h6><b>Years of experience:</b> 27 years</h6>
+      <h6><b>Qualifications:</b> MBBS (Manipal), M Psych Med (UM)</h6>
+      <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
+      Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
+      Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+    </Card.Text>
+  </Card.Body>
 
-        
+  <Card.Body>
+    <Card.Link href="#">Book Appointment</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+          </Card>
+
+          <Card style={{ width: '100rem', height: '20rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Dr Tee Bee Chin</Card.Title>
+    <Card.Text>
+      <h6><b>Specialisation:</b> Psychiatry</h6>
+      <h6><b>Years of experience:</b>  21 Years</h6>
+      <h6><b>Qualifications:</b> MD (UKM), Mmed. Psych (UKM),
+       Subspeciality & Fellowship in Child & Adolescent Psychiatry (Singapore).</h6>
+      <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
+      Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
+      Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+    </Card.Text>
+  </Card.Body>
+
+  <Card.Body>
+    <Card.Link href="#">Book Appointment</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+          </Card>
+
+
+          <Card style={{ width: '100rem', height: '20rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Dr Lim Wai Jenn</Card.Title>
+    <Card.Text>
+      <h6><b>Specialisation:</b> Psychiatry</h6>
+      <h6><b>Years of experience:</b>  14 Years </h6>
+      <h6><b>Qualifications:</b> MD (Jefferson, USA), ABPN Psychiatry (USA), ABPN Child & Adolescent Psychiatry (USA),
+       Fellowship in Intensive Structural Family Therapy (USA)</h6>
+      <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
+      Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
+      Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+    </Card.Text>
+  </Card.Body>
+
+  <Card.Body>
+    <Card.Link href="#">Book Appointment</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+          </Card>
+
+
+          <Card style={{ width: '100rem', height: '20rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Dr Khoo Yee Laim</Card.Title>
+    <Card.Text>
+      <h6><b>Specialisation:</b> Psychiatry</h6>
+      <h6><b>Years of experience:</b>  24 Years </h6>
+      <h6><b>Qualifications:</b> MD (USM), MPM (UM), MASTER OF PSYCHOLOGY, MEDICINE (UM)</h6>
+      <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
+      Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
+      Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+    </Card.Text>
+  </Card.Body>
+
+  <Card.Body>
+    <Card.Link href="#">Book Appointment</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+          </Card>
+
+          <Card style={{ width: '100rem', height: '20rem'}}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Dr Aw Tui lar (T.I. Wong)</Card.Title>
+    <Card.Text>
+      <h6><b>Specialisation:</b> Psychiatry</h6>
+      <h6><b>Years of experience:</b>  37 Years </h6>
+      <h6><b>Qualifications:</b> MBChB (UK) , MRCPsych (UK)</h6>
+      <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
+      Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
+      Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+    </Card.Text>
+  </Card.Body>
+
+  <Card.Body>
+    <Card.Link href="#">Book Appointment</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+          </Card>
     </div>
     );
 }
