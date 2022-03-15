9eb68c92815585a10346799e0386eb42543368c6
danesshmathura <danessh75@gmail.com>
2022-03-15 15:55:32 +0800

    update fertality

--- src/components/specialism/Fertality.jsx
+++ src/components/specialism/Fertality.jsx
@@ -6,43 +6,23 @@ import image1 from "../../assets/login_page.jpg";
 
 function Fertality() {
     return (
-        <div className="body-container" style={{backgroundImage: 'none', backgroundColor: 'white'}}>
-        <Navbar />
+      <div className="body-container" style={{backgroundImage: 'none', backgroundColor: 'white'}}>
+      <Navbar />
 
-        <h1>List of Fertality Doctors</h1>
-         
-        <Card style={{ width: '100rem', height: '20rem'}}>
-<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-<Card.Body>
-<Card.Title>Dr Ting Joe Hang</Card.Title>
-<Card.Text>
-  <h6><b>Specialisation:</b> Psychiatry</h6>
-  <h6><b>Years of experience:</b> 27 years</h6>
-  <h6><b>Qualifications:</b> MBBS (Manipal), M Psych Med (UM)</h6>
-  <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
-  Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
-  Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
-</Card.Text>
-</Card.Body>
-
-<Card.Body>
-<Card.Link href="#">Book Appointment</Card.Link>
-<Card.Link href="#">Another Link</Card.Link>
-</Card.Body>
-      </Card>
-
+      <h1>List of Gynaecologist Doctors</h1>
+       
       <Card style={{ width: '100rem', height: '20rem'}}>
 <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
 <Card.Body>
-<Card.Title>Dr Tee Bee Chin</Card.Title>
+<Card.Title>Dr Navdeep Singh Pannu</Card.Title>
 <Card.Text>
-  <h6><b>Specialisation:</b> Psychiatry</h6>
-  <h6><b>Years of experience:</b>  21 Years</h6>
-  <h6><b>Qualifications:</b> MD (UKM), Mmed. Psych (UKM),
-   Subspeciality & Fellowship in Child & Adolescent Psychiatry (Singapore).</h6>
-  <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
-  Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
-  Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 22 years</h6>
+<h6><b>Qualifications:</b>M.B.B.S. (India), M.MED, O&G (Malaya), M.R.C.O.G. (London), A.M. (Malaysia),
+ Fellow International College of Surgeons (U.S.A.) Cert.</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
 </Card.Text>
 </Card.Body>
 
@@ -50,21 +30,20 @@ function Fertality() {
 <Card.Link href="#">Book Appointment</Card.Link>
 <Card.Link href="#">Another Link</Card.Link>
 </Card.Body>
-      </Card>
+    </Card>
 
-
-      <Card style={{ width: '100rem', height: '20rem'}}>
+    <Card style={{ width: '100rem', height: '20rem'}}>
 <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
 <Card.Body>
-<Card.Title>Dr Lim Wai Jenn</Card.Title>
+<Card.Title>Dr Chong Kuoh Ren</Card.Title>
 <Card.Text>
-  <h6><b>Specialisation:</b> Psychiatry</h6>
-  <h6><b>Years of experience:</b>  14 Years </h6>
-  <h6><b>Qualifications:</b> MD (Jefferson, USA), ABPN Psychiatry (USA), ABPN Child & Adolescent Psychiatry (USA),
-   Fellowship in Intensive Structural Family Therapy (USA)</h6>
-  <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
-  Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
-  Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 20 years</h6>
+<h6><b>Qualifications:</b>MBBS (UM), MRCOG (UK), F.MAS, D.MAS,
+ M.Cert. Reproductive Medicine & IVF (NHS)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
 </Card.Text>
 </Card.Body>
 
@@ -72,20 +51,19 @@ function Fertality() {
 <Card.Link href="#">Book Appointment</Card.Link>
 <Card.Link href="#">Another Link</Card.Link>
 </Card.Body>
-      </Card>
+    </Card>
 
-
-      <Card style={{ width: '100rem', height: '20rem'}}>
+    <Card style={{ width: '100rem', height: '20rem'}}>
 <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
 <Card.Body>
-<Card.Title>Dr Khoo Yee Laim</Card.Title>
+<Card.Title>Dr Thaneemalai Jeganathan</Card.Title>
 <Card.Text>
-  <h6><b>Specialisation:</b> Psychiatry</h6>
-  <h6><b>Years of experience:</b>  24 Years </h6>
-  <h6><b>Qualifications:</b> MD (USM), MPM (UM), MASTER OF PSYCHOLOGY, MEDICINE (UM)</h6>
-  <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
-  Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
-  Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 29 years</h6>
+<h6><b>Qualifications:</b>MBBS(IND) , MOBGYN, AM(MAL).</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
 </Card.Text>
 </Card.Body>
 
@@ -93,19 +71,20 @@ function Fertality() {
 <Card.Link href="#">Book Appointment</Card.Link>
 <Card.Link href="#">Another Link</Card.Link>
 </Card.Body>
-      </Card>
+    </Card>
 
-      <Card style={{ width: '100rem', height: '20rem'}}>
+
+    <Card style={{ width: '100rem', height: '20rem'}}>
 <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
 <Card.Body>
-<Card.Title>Dr Aw Tui lar (T.I. Wong)</Card.Title>
+<Card.Title>Dr Arifah Mohd Amin @ Hamim</Card.Title>
 <Card.Text>
-  <h6><b>Specialisation:</b> Psychiatry</h6>
-  <h6><b>Years of experience:</b>  37 Years </h6>
-  <h6><b>Qualifications:</b> MBChB (UK) , MRCPsych (UK)</h6>
-  <h6><b>Condition Consulted:</b> Mania, Sleep Disorders, Antisocial Behavior, Bipolar Disorder, Hypersomnia, Hyperactivity Disorder, Bad Dreams, Trichotillomania, 
-  Masturbation Addiction, Eating Disorders, Double Personality, Suicidal Behavior, Selective Mutism, Stress, Oppositional Defiant Disorder, Drug Abuse, School Refusal,
-  Drug Overdose, Psychological Problems, Post Traumatic Stress Disorder (PTSD) and Pervasive Developmental Disorders (PDD)</h6>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 23 years</h6>
+<h6><b>Qualifications:</b>MBBS(India) , M.MED O&G(UKM)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
 </Card.Text>
 </Card.Body>
 
@@ -113,7 +92,128 @@ function Fertality() {
 <Card.Link href="#">Book Appointment</Card.Link>
 <Card.Link href="#">Another Link</Card.Link>
 </Card.Body>
-      </Card>
+    </Card>
+
+    <Card style={{ width: '100rem', height: '20rem'}}>
+<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+<Card.Body>
+<Card.Title>Dr Nurhazinat Mohamed Yunus</Card.Title>
+<Card.Text>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 24 years</h6>
+<h6><b>Qualifications:</b>MBBS (UM) , Master in O&G (UKM)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
+</Card.Text>
+</Card.Body>
+
+<Card.Body>
+<Card.Link href="#">Book Appointment</Card.Link>
+<Card.Link href="#">Another Link</Card.Link>
+</Card.Body>
+    </Card>
+
+    <Card style={{ width: '100rem', height: '20rem'}}>
+<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+<Card.Body>
+<Card.Title>Dr Surinder Singh</Card.Title>
+<Card.Text>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 27 years</h6>
+<h6><b>Qualifications:</b>MBBS , MMed , O & G (Malaysia) , AM , FAGE ,
+ Fellow of International College of Surgeons (USA)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
+</Card.Text>
+</Card.Body>
+
+<Card.Body>
+<Card.Link href="#">Book Appointment</Card.Link>
+<Card.Link href="#">Another Link</Card.Link>
+</Card.Body>
+    </Card>
+    
+    <Card style={{ width: '100rem', height: '20rem'}}>
+<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+<Card.Body>
+<Card.Title>Dr Wai Kok Yau</Card.Title>
+<Card.Text>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 17 years</h6>
+<h6><b>Qualifications:</b>MBBS (UM) , MRCOG (UK)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
+</Card.Text>
+</Card.Body>
+
+<Card.Body>
+<Card.Link href="#">Book Appointment</Card.Link>
+<Card.Link href="#">Another Link</Card.Link>
+</Card.Body>
+    </Card>
+
+    <Card style={{ width: '100rem', height: '20rem'}}>
+<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+<Card.Body>
+<Card.Title>Dr Wong Chin Yuan</Card.Title>
+<Card.Text>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 33 years</h6>
+<h6><b>Qualifications:</b>MBChB (Wales), DFFP (UK), MRCOG (UK)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
+</Card.Text>
+</Card.Body>
+
+<Card.Body>
+<Card.Link href="#">Book Appointment</Card.Link>
+<Card.Link href="#">Another Link</Card.Link>
+</Card.Body>
+    </Card>
+
+    <Card style={{ width: '100rem', height: '20rem'}}>
+<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+<Card.Body>
+<Card.Title>Dato Dr Sivamohan N.</Card.Title>
+<Card.Text>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 43 years</h6>
+<h6><b>Qualifications:</b>MBBS (India-under IRRC scholarship), MRCOG (UK), FELLOWSHIP IN GYNAE-ONCOLOGY (UK), FICS (US)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
+</Card.Text>
+</Card.Body>
+
+<Card.Body>
+<Card.Link href="#">Book Appointment</Card.Link>
+<Card.Link href="#">Another Link</Card.Link>
+</Card.Body>
+    </Card>
+
+    <Card style={{ width: '100rem', height: '20rem'}}>
+<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+<Card.Body>
+<Card.Title>Dr Milton Lum Siew Wah</Card.Title>
+<Card.Text>
+<h6><b>Specialisation:</b> Fertality </h6>
+<h6><b>Years of experience:</b> 49 years</h6>
+<h6><b>Qualifications:</b>MBBS (UM), MRCOG (UK), FRCOG (UK)</h6>
+<h6><b>Condition Consulted:</b> Lactational, Vulvar Cancer, Ectopic Pregnancy, Menopause, Period Pain, Polyps, Listeriosis, HPV, Vaginal Prolapse,
+ Pregnancy And Heart Disease, Bleeding Problems, Dysmenorrhoea, Rubella, Ovarian Cysts, Ovarians Cysts, White Discharge, Genital Sores, Irregular Menstruation,
+ Twins, Genital Injury, UTI and Amenorrhoea</h6>
+</Card.Text>
+</Card.Body>
+
+<Card.Body>
+<Card.Link href="#">Book Appointment</Card.Link>
+<Card.Link href="#">Another Link</Card.Link>
+</Card.Body>
+    </Card>
 </div>
     );
 }
