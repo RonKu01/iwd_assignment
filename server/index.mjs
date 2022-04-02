f30722a339d945d6cb6f5b6ffa66ba6767c42131
RonKu01 <87351766+RonKu01@users.noreply.github.com>
2022-04-02 13:07:25 +0800

    2 April 1pm Comment for back end done

--- server/index.mjs
+++ server/index.mjs
@@ -1,33 +1,25 @@
 import express from "express";
 import mysql from "mysql";
-
 import jwt from "jsonwebtoken";
 import cors from "cors";
-
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-
 import bcrypt from "bcrypt";
-
 const saltRounds = 10;
-
 const app = express();
-const VIDEOSDK_API_ENDPOINT= "https://api.videosdk.live"
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
     cors({
         origin: ["http://localhost:3000"],
+        //Only allow to use get, post, and put method
         methods: ["GET", "POST", "PUT"],
         credentials: true,
     })
 );
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(
     session({
         key: "userID",
@@ -43,16 +35,19 @@ app.use(
 const db = mysql.createConnection({
     host: "sql6.freemysqlhosting.net", //"localhost",
     user: "sql6479845", //"root",
-    password: "bdiIe3fDLc",
+    password: "bdiIe3fDLc", //"",
     database: "sql6479845",
 });
 
+//Declare session variable
 let sess, sess_loginId, sess_appointmentId;
 
+// Print Hello World (testing)
 app.get('/', (req,res)=>{
     res.send("hello world");
 })
 
+// API to set the session variable when users logged in.
 app.get("/login", (req, res) => {
     if (sess === true) {
         res.send({ loggedIn: true, loginID: sess_loginId });
@@ -61,6 +56,7 @@ app.get("/login", (req, res) => {
     }
 });
 
+//API to check whether user credentials is valid or not. (Login Function)
 app.post("/login", ( req, res) =>{
     sess = req.session;
     sess_loginId = req.session.loginId;
@@ -92,6 +88,7 @@ app.post("/login", ( req, res) =>{
     );
 });
 
+//API to clear session variable (Logout function)
 app.get("/logout", (req, res) => {
     req.session.destroy((err) => {
         sess = null;
@@ -104,6 +101,7 @@ app.get("/logout", (req, res) => {
     })
 })
 
+//API to get all patient data
 app.get('/getPatData', (req,res)=>{
     db.query("SELECT * FROM `login` INNER JOIN `patient` ON login.loginID = patient.loginID ORDER BY login.loginID ASC",
 
@@ -114,6 +112,7 @@ app.get('/getPatData', (req,res)=>{
     );
 })
 
+//API to call a post request to store patient data into database (Register Patient Account Function)
 app.post("/patRegister", (req, res)=>{
     const fullName = req.body.fullName
     const dob = req.body.dob
@@ -155,6 +154,7 @@ app.post("/patRegister", (req, res)=>{
     });
 });
 
+//API to call a put request to update patient data into database (Update Patient Profile Function)
 app.put("/updatePat",(req, res)=>{
     const id = req.body.id
     const address = req.body.address
@@ -189,7 +189,7 @@ app.put("/updatePat",(req, res)=>{
 
 })
 
-//Doctor
+//API to get all doctor data
 app.get('/getDocData', (req,res)=>{
     db.query("SELECT * FROM `doctor` INNER JOIN `login` ON doctor.loginID = login.loginID INNER JOIN `specialisation` ON doctor.specialisationID = specialisation.specialisationID ORDER BY doctor.doctorID ASC",
         function (err, result) {
@@ -199,6 +199,7 @@ app.get('/getDocData', (req,res)=>{
     );
 })
 
+//API to call a post request to store Doctor data into database (Register Doctor Account Function)
 app.post("/docRegister", (req, res)=>{
     const docSpec = req.body.specialisationID
     const docFullName = req.body.doctorName
@@ -242,6 +243,7 @@ app.post("/docRegister", (req, res)=>{
     });
 });
 
+//API to call a put request to update Doctor data into database (Update Doctor Profile Function)
 app.put("/updateDoc",(req, res)=>{
     const id = req.body.id
     const docName = req.body.doctorName
@@ -288,6 +290,7 @@ app.put("/updateDoc",(req, res)=>{
 
 })
 
+//API to get all Specialism data
 app.get('/getSpecialism', (req,res)=>{
     db.query("SELECT * FROM `specialisation` ORDER BY specialisation.specialisationID ASC",
         function (err, result) {
@@ -297,6 +300,7 @@ app.get('/getSpecialism', (req,res)=>{
     );
 })
 
+//API to get doctor data by filtering with doctor.loginId
 app.get("/getPersonDoctorData", ( req, res) =>{
     db.query("SELECT * FROM login INNER JOIN doctor ON login.loginId = doctor.loginId INNER JOIN specialisation ON doctor.specialisationID = specialisation.specialisationID WHERE doctor.loginId = ?",
         [sess_loginId],
@@ -311,6 +315,7 @@ app.get("/getPersonDoctorData", ( req, res) =>{
     );
 });
 
+//API to get patient data by filtering with patient.loginId
 app.get("/getPersonPatData", ( req, res) =>{
     db.query("SELECT * FROM login INNER JOIN patient ON login.loginId = patient.loginId WHERE patient.loginId = ?",
         [sess_loginId],
@@ -325,6 +330,7 @@ app.get("/getPersonPatData", ( req, res) =>{
     );
 });
 
+//API to get doctor data by filtering with doctor.SpecialismID
 app.post("/getDoctorBySpecialism", ( req, res) =>{
 
     const specialisationID = req.body.specialisationID
@@ -342,6 +348,7 @@ app.post("/getDoctorBySpecialism", ( req, res) =>{
     );
 });
 
+//API to call a post request to store appointment data into database (Patient Book Appointment Function)
 app.post("/registerAppointment", (req, res)=>{
 
     const loginID = sess_loginId
@@ -374,6 +381,7 @@ app.post("/registerAppointment", (req, res)=>{
     );
 })
 
+//API to get 'Pending' appointment data by filtering with appointment.status (status != Done And status != Canceled)
 app.get("/getPatListByDoc", ( req, res) =>{
     db.query("SELECT * FROM appointment INNER JOIN doctor ON doctor.doctorID = appointment.doctorID  INNER JOIN patient ON appointment.patID = patient.patID WHERE doctor.loginId = ? AND status != 'Done' AND status != 'Cancelled' ORDER BY appointmentDate DESC",
         [sess_loginId],
@@ -388,8 +396,8 @@ app.get("/getPatListByDoc", ( req, res) =>{
     );
 });
 
+//API to get Consultation history from database
 app.get("/getConsultHistoryByDoc", ( req, res) =>{
-
     db.query("SELECT * FROM appointment INNER JOIN feedback ON feedback.appointmentID = appointment.appointmentID INNER JOIN doctor ON doctor.doctorID = appointment.doctorID  INNER JOIN patient ON appointment.patID = patient.patID INNER JOIN consultation ON consultation.appointmentID = appointment.appointmentID WHERE doctor.loginId = ? ORDER BY appointmentDate DESC",
         [sess_loginId],
         function (err, result) {
@@ -403,6 +411,7 @@ app.get("/getConsultHistoryByDoc", ( req, res) =>{
     );
 });
 
+//API to get 'Pending' appointment data by filtering with appointment.status (status != Done) AND patient.loginID
 app.get("/getPatAppointment", ( req, res) =>{
     db.query("SELECT * FROM appointment INNER JOIN patient ON patient.patID = appointment.patID INNER JOIN doctor ON appointment.doctorID = doctor.doctorID  WHERE patient.loginId = ? AND status != 'Done' ORDER BY appointmentDate DESC",
         [sess_loginId],
@@ -417,6 +426,7 @@ app.get("/getPatAppointment", ( req, res) =>{
     );
 });
 
+//API to get meetingID (Patient Join Meeting Function)
 app.get("/getPatAppointmentByAppointmentID", ( req, res) =>{
 
     db.query("SELECT * FROM appointment INNER JOIN patient ON patient.patID = appointment.patID  WHERE patient.loginId = ? AND appointment.appointmentID = ? ORDER BY appointmentDate DESC",
@@ -431,6 +441,7 @@ app.get("/getPatAppointmentByAppointmentID", ( req, res) =>{
     );
 });
 
+//API to get patient data + appointment data for the doctor by filtering with doctor.loginID
 app.get("/patient", ( req, res) =>{
     db.query("SELECT * FROM appointment INNER JOIN patient ON patient.doctorID = appointment.doctorID  INNER JOIN patient ON appointment.patID = patient.patID WHERE doctor.loginId = ? ORDER BY appointmentDate DESC",
         [sess_loginId],
@@ -445,6 +456,7 @@ app.get("/patient", ( req, res) =>{
     );
 });
 
+//API to call a put request to update appointment status into database
 app.put("/updateStatus",(req, res)=>{
     const status = req.body.status
     const appointmentID = req.body.appointmentID
@@ -460,6 +472,7 @@ app.put("/updateStatus",(req, res)=>{
     )
 })
 
+//API to get the auto-generated tokenID
 app.get("/get-token", (req, res) => {
     const API_KEY = "9c2d8fef-3ffd-494c-aa06-8f9d3c9ddb7a";
     const SECRET_KEY = "d618b715e8cc4d194288f8f3b4f73a33c7dc5b3b541b898a0f4802bb4ba4f59f";
@@ -472,11 +485,13 @@ app.get("/get-token", (req, res) => {
     res.json({ token });
 });
 
+//API to set appointmentID to session.
 app.put("/updateMeetingDetails",(req, res)=>{
     sess_appointmentId = req.body.appointmentID;
     res.send({message : "Success"});
 })
 
+//API to call a put request to update meetingID and token into database
 app.put("/updateMeetingId",(req, res)=>{
     const token = req.body.token
     const meetingID = req.body.meetingID
@@ -493,10 +508,12 @@ app.put("/updateMeetingId",(req, res)=>{
     )
 })
 
+//API to get appointmentId from the session(appointmentID)
 app.get("/getMeetingDetails", (req, res) => {
     res.send({ appointmentId: sess_appointmentId });
 });
 
+//API to check whether the MeetingID is exist or not
 app.post("/getMeetingDetails", ( req, res) =>{
     const appointmentId = req.body.appointmentId
 
@@ -516,6 +533,7 @@ app.post("/getMeetingDetails", ( req, res) =>{
     );
 });
 
+//API to call a post request to store diagnosis and treatment data into database (Doctor Submit Consultation Form Function)
 app.post("/addConsultation", ( req, res) =>{
     const appointmentId = sess_appointmentId;
     const dignosis = req.body.diagnosis;
@@ -549,6 +567,7 @@ app.post("/addConsultation", ( req, res) =>{
     );
 });
 
+//API to get 'Done' appointment data by filtering with patient.loginID
 app.get("/getPatAppointmentDone", ( req, res) =>{
     db.query("SELECT * FROM appointment INNER JOIN feedback ON feedback.appointmentID = appointment.appointmentID INNER JOIN patient ON patient.patID = appointment.patID INNER JOIN doctor ON appointment.doctorID = doctor.doctorID INNER JOIN consultation ON consultation.appointmentID = appointment.appointmentID  WHERE patient.loginId = ? AND status = 'Done' ORDER BY appointmentDate DESC",
         [sess_loginId],
@@ -563,7 +582,7 @@ app.get("/getPatAppointmentDone", ( req, res) =>{
     );
 });
 
-//database need to change column
+//API to call a put request to update feedback into database
 app.put("/addFeedback", ( req, res) =>{
     const appointmentId = req.body.appointmentID;
     const feedback = req.body.feedback;
@@ -583,6 +602,7 @@ app.put("/addFeedback", ( req, res) =>{
     );
 });
 
+//API to check whether the server is running or not
 app.listen(3005 , () => {
     console.log('running on port 3005')
 
